d19632cf95e1cd44c10e7d8e48e83953655319b2
Gayathri-747 <gayathri.sg747@gmail.com>
2025-12-28 12:01:48 +0530

    changed variable a to 10

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 class test{
     public static void main(String[] args){
         System.out.println("Hello Test");
-        int a = 9;
+        int a = 10;
         System.out.println("Hello Test 2 "+a);
         System.out.println("edit3");
     }
